6cdbf409b85e551ac2040a38973fb7d5125d160d
illegal-instuction-co <79029454+illegal-instuction-co@users.noreply.github.com>
2021-12-14 21:32:11 +0300

    Initial commit

---
+++ EasySafe.cpp
@@ -0,0 +1,5 @@
+#include <EasySafe.h>
+
+void main() {
+
+}
---
+++ EasySafe.h
@@ -0,0 +1,19 @@
+/*
+* Copyright 2021 ILLEGAL-INSTRUCTION-CO
+* 
+* I am too lazy to find a suitable license for this app. 
+* You can do whatever you want with this app and you don't have to mention my name. 
+* Life is really hard, and sometimes we need to make it easy for each other. Bye babe.
+* -----------------
+* MBK
+* -----------------
+*/
+
+/*
+* Inline syscalls.
+* -----------------
+* https://j00ru.vexillium.org/syscalls/nt/64/
+* -----------------
+* Avoiding instrumentation callbacks a.k.a. syscall callbacks.
+* https://bestofcpp.com/repo/Deputation-instrumentation_callbacks-cpp-utilities
+*/
